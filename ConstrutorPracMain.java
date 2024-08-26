b9e1c6bb91ef219e53f97c244803b695044115c0
Ravi Shekhram <pshekhram@gmail.com>
2024-08-26 18:28:00 -0400

    Your commit message

---
+++ ConstrutorPracMain.java
@@ -0,0 +1,14 @@
+package practicepolymorphism;
+
+public class ConstrutorPracMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ConstrutorPrac c= new ConstrutorPrac();
+		//ConstrutorPrac c= new ConstrutorPrac(10.3, 20.4,12.6);
+		//ConstrutorPrac c=new ConstrutorPrac(20.4);
+		ConstrutorPrac c= new ConstrutorPrac(38.8);
+		System.out.println(c.volume());
+	}
+
+}
---
+++ PolymorphismPracticMain.java
@@ -0,0 +1,15 @@
+package practicepolymorphism;
+
+public class PolymorphismPracticMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PolymorphismPracti abcd = new PolymorphismPracti();
+		abcd.sum();
+		abcd.sum(10,20);
+		abcd.sum(10,20.0);
+		abcd.sum(20.20,20);
+
+	}
+
+}
